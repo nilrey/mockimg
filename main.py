aee2bb8be9f2fb55571623a7c0f8806f0b467e2f
nil-laptop <nilrey@gmail.com>
2024-08-16 16:17:52 +0300

    рабочий вариант 1 на локальной машине

--- main.py
+++ main.py
@@ -4,30 +4,27 @@ import glob
 import requests
 
 
-file_path = '/etc/hostname'
-hostname = 'undefined'
-with open(file_path, "r") as file:
-    hostname = file.read().strip()
+hostname_path = '/etc/hostname'
+output_path = "" # "/code/output/"
+input_path = '/code/input/'
 
-# path = "/code/output/"
-# with open("api_get_hostname.txt", "w+") as f:
-#     f.write('{"name": "' + hostname + '", "time": "' + str(dt.datetime.now()) + '"}')
-# time.sleep(60)
+def getContainerId(containerId = 0):
+    with open(hostname_path, "r") as file:
+        containerId = file.read().strip()
+    return containerId
 
-
-folder_path = '/code/input/'
-filesPaths = glob.glob(folder_path + '*')
-for fpath in filesPaths :
-    fname = fpath.replace(folder_path, '').replace('.mp4', '.json')
-    time.sleep(3)
-    path = "/code/output/"
-    # path = ""
-    with open(path + fname, "w+") as f:
-        f.write('{"filename": "' + fname + '", "hostname": "' + hostname + '", "time": "' + str(dt.datetime.now()) + '"}')
+containerId = getContainerId()
+url = f'http://127.0.0.1:8002/events/{containerId}/before_start'
+myobj = {"msg": "before ann start", "details": "" }
+before_start_response = requests.post(url, json = myobj)
 
 
-    print(x.text)
+with open(output_path + "before_start.txt", "w+") as f:
+    f.write('{"action": "before_start", "containerId": "' + containerId + '", "time": "' + str(dt.datetime.now()) + '", "response": ' + before_start_response.text + '}')
 
-
-
-print("Finish")
+# filesPaths = glob.glob(input_path + '*')
+# for fpath in filesPaths :
+#     fname = fpath.replace(input_path, '').replace('.mp4', '.json')
+#     time.sleep(3)
+#     with open(output_path + fname, "w+") as f:
+#         f.write('{"filename": "' + fname + '", "hostname": "' + hostname + '", "time": "' + str(dt.datetime.now()) + '"}')
