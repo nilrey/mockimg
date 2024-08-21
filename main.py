98ad1818c592c6a3c80197723a7cfd35fc820002
nil-laptop <nilrey@gmail.com>
2024-08-21 16:17:19 +0300

    обновление для локального тестирования

--- main.py
+++ main.py
@@ -10,18 +10,22 @@ mockjson = 'mockjson.json'
 mockjson_content = '{}'
 logfile = 'processing.log'
 hostname_path = '/etc/hostname'
-input_path = '/code/input/'
+input_path = '/code/input/video/'
 output_path = '/code/output/'
-containerId = 0
+containerId = '0000000'
 
 
-def getUrl():
-    return f'http://{domain}/events/{containerId}/before_start'
+def getUrl(dom='camino-restapi', cId=''):
+    return f'http://{dom}/events/{cId}/before_start'
 
 
 def setMessage(msg = ""):
     return {"msg": msg }
 
+def writeLog(action = "", txt = ""):
+    msg = '{"action": "'+ action +'", "containerId": "' + containerId + '", "response": "'+ txt +'", "time": "' + str(dt.datetime.now()) + '"}'
+    with open(output_path + logfile, "a+") as f:
+        f.write(msg + '\n')
 
 with open(mockjson, "r") as file:
     mockjson_content = file.read()
@@ -29,23 +33,22 @@ with open(mockjson, "r") as file:
 with open(hostname_path, "r") as file:
     containerId = file.read().strip()
 
+print("Begin")
 
-with open(output_path + logfile, "w+") as f:
-    f.write(getUrl())
+writeLog("url_compose", getUrl(domain, containerId))
 
-before_start_response = requests.post(getUrl(), json = setMessage('before ann start') )
+before_start_response = requests.post(getUrl(domain, containerId), json = setMessage('before ann start') )
 
-# вывод response от camino-restapi в сторонний файл, размещен на хосте в соответствующей папке output данного проекта
-with open(output_path + logfile, "w+") as f:
-    f.write('{"action": "before_start", "containerId": "' + containerId + 
-            '", "time": "' + str(dt.datetime.now()) +
-            '", "response": ' + before_start_response.text + '}')
+writeLog("before_start", before_start_response.text)
 
-
+# читаем файлы из директории video
 filesPaths = glob.glob(input_path + '*')
 for fpath in filesPaths :
     fname = fpath.replace(input_path, '').replace('.mp4', '.json')
-    time.sleep(3)
     with open(output_path + fname, "w+") as f:
         f.write(mockjson_content)
-        # f.write('{"filename": "' + fname + '", "containerId": "' + containerId + '", "time": "' + str(dt.datetime.now()) + '"}')
+        writeLog("procceed", "filename: "+fname)
+        time.sleep(3)
+
+writeLog("before_end", "success")
+print("End")
